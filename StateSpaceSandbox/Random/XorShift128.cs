0178d515299e3c2a0f1c1658699c1f55e196b9e5
Markus Mayer <development@widemeadows.de>
2012-12-07 01:23:37 +0100

    Improved XorShift160

--- StateSpaceSandbox/Random/XorShift128.cs
+++ StateSpaceSandbox/Random/XorShift128.cs
@@ -41,6 +41,8 @@ namespace StateSpaceSandbox.Random
         /// <returns>The random number</returns>
         public int Next()
         {
+            // http://www.jstatsoft.org/v08/i14/paper
+
             uint t = _x ^ (_x << 11);
             _x = _y; _y = _z; _z = _w;
             _w ^= (_w >> 19) ^ t ^ (t >> 8);
--- StateSpaceSandbox/Random/XorShift160.cs
+++ StateSpaceSandbox/Random/XorShift160.cs
@@ -7,17 +7,18 @@ namespace StateSpaceSandbox.Random
     /// </summary>
     public sealed class XorShift160
     {
-        private uint _x;
-        private uint _y;
-        private uint _z; 
-        private uint _w;
-        private uint _v;
+        private ulong _x;
+        private ulong _y;
+        private ulong _z;
+        private ulong _w;
+        private ulong _v;
+        private ulong _d;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="XorShift160" /> class.
         /// </summary>
         public XorShift160()
-            : this(123456789 ^ Environment.TickCount, 362436069 ^ Environment.TickCount, 521288629 ^ Environment.TickCount, 88675123 ^ Environment.TickCount, Environment.TickCount)
+            : this(123456789 ^ Environment.TickCount, 362436069 ^ Environment.TickCount, 521288629 ^ Environment.TickCount, 88675123 ^ Environment.TickCount, 5783321 ^ Environment.TickCount, 6615241 ^ Environment.TickCount)
         {
         }
 
@@ -28,13 +29,33 @@ namespace StateSpaceSandbox.Random
         /// <param name="seedY">The seed Y.</param>
         /// <param name="seedZ">The seed Z.</param>
         /// <param name="seedW">The seed W.</param>
-        public XorShift160(int seedX, int seedY, int seedZ, int seedW, int seedV)
+        public XorShift160(long seedX, long seedY, long seedZ, long seedW, long seedV, long seedD)
         {
             _x = (uint)seedX;
             _y = (uint)seedY;
             _z = (uint)seedZ;
             _w = (uint)seedW;
             _v = (uint)seedV;
+            _d = (uint)seedD;
+        }
+
+        /// <summary>
+        /// Gets the next integer value
+        /// </summary>
+        /// <returns>The random number</returns>
+        public long NextLong()
+        {
+            // http://www.jstatsoft.org/v08/i14/paper
+
+            ulong t = _x ^ (_x >> 2);
+            _x = _y;
+            _y = _z;
+            _z = _w;
+            _w = _v;
+            _v = (_v ^ (_v << 2)) ^ (t ^ (t << 1));
+            _d += 362437 + _v;
+
+            return (long)_d;
         }
 
         /// <summary>
@@ -43,18 +64,7 @@ namespace StateSpaceSandbox.Random
         /// <returns>The random number</returns>
         public int Next()
         {
-            const int a = 7;
-            const int b = 13;
-            const int c = 6;
-
-            uint t = _x ^ (_x << a);
-            _x = _y; 
-            _y = _z; 
-            _z = _w;
-            _w = _v;
-            _v = (_v ^ (_v >> c)) ^ (t ^ (t >> b));
-
-            return (int)_v;
+            return (int)NextLong();
         }
 
         /// <summary>
@@ -63,8 +73,8 @@ namespace StateSpaceSandbox.Random
         /// <returns>The random number</returns>
         public double NextDouble()
         {
-            uint rnd = (uint) Next();
-            double scaling = 1.0D/UInt32.MaxValue;
+            ulong rnd = (ulong)NextLong();
+            double scaling = 1.0D/UInt64.MaxValue;
             double value = 2.0D * (rnd * scaling - 0.5D);
             return value;
         }
